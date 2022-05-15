b250d52347c49a232dd24970b9a52aec96b1af9c
mikkolaj <bulmikolaj@interia.pl>
2022-05-15 15:11:12 +0200

    moved backend, added lombok

--- web-app-backend/src/main/java/com/swozo/SwozoApplication.java
+++ web-app-backend/src/main/java/com/swozo/SwozoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SwozoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SwozoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SwozoApplication.class, args);
+    }
 
 }
