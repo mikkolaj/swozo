0dc8eb0a182d885715e0f9ca6e596b34c1993f51
mikolaj <bulmikolaj@interia.pl>
2022-05-22 08:50:17 +0200

    naprawienie zwracania wyniku tworzenia/usuwania instancji

--- orchestrator/src/main/java/com/swozo/orchestrator/OrchestratorApplication.java
+++ orchestrator/src/main/java/com/swozo/orchestrator/OrchestratorApplication.java
@@ -1,13 +1,69 @@
 package com.swozo.orchestrator;
 
+import com.swozo.orchestrator.gcloud.compute.VMLifecycleManager;
+import com.swozo.orchestrator.gcloud.compute.model.VMAddress;
+import com.swozo.orchestrator.gcloud.compute.model.VMSpecs;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class OrchestratorApplication {
+    @Autowired
+    VMLifecycleManager vmLifecycleManager;
+
+    Logger logger = LoggerFactory.getLogger(OrchestratorApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(OrchestratorApplication.class, args);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void doSomethingAfterStartup() {
+        System.out.println("hello world, I have just started up");
+        createAndDeleteInstance();
+    }
+
+    public void createAndDeleteInstance() {
+        var project = "hybrid-text-350213";
+        var zone = "us-west2-a";
+        var networkName = "default";
+        var vmName = "super-instancja";
+
+        var vmAddress = new VMAddress(project, zone, networkName, vmName);
+        var vmSpecs = new VMSpecs("e2-micro", "debian-11", 10);
+
+        try {
+            var operation = vmLifecycleManager.createInstance(vmAddress, vmSpecs);
+            var response = operation.get(3, TimeUnit.MINUTES);
+
+            if (response.hasError()) {
+                logger.error("Instance creation failed! " + response);
+            } else {
+                logger.info("Operation Status: " + response.getStatus());
+            }
+        } catch (Exception exception) {
+            logger.error("Failed to create instance. " + vmAddress + " " + exception);
+        }
+
+        try {
+            var operation = vmLifecycleManager.deleteInstance(vmAddress);
+            var response = operation.get(3, TimeUnit.MINUTES);
+
+            if (response.hasError()) {
+                logger.error("Instance creation failed! " + response);
+            } else {
+                logger.info("Operation Status: " + response.getStatus());
+            }
+        } catch (Exception exception) {
+            logger.error("Failed to delete instance. " + vmAddress + " " + exception);
+        }
     }
 
 }
--- orchestrator/src/main/java/com/swozo/orchestrator/gcloud/compute/VMLifecycleManager.java
+++ orchestrator/src/main/java/com/swozo/orchestrator/gcloud/compute/VMLifecycleManager.java
@@ -10,11 +10,15 @@ import com.swozo.orchestrator.gcloud.compute.providers.storage.DiskProvider;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 
 @Service
@@ -22,14 +26,16 @@ import java.util.concurrent.TimeUnit;
 public class VMLifecycleManager {
     private static final String defaultDiskName = "disk-1";
 
+    private static final int GCLOUD_TIMEOUT_MINUTES = 3;
+
     private final DiskProvider diskProvider;
     private final NetworkInterfaceProvider networkInterfaceProvider;
     private final InstanceProvider instanceProvider;
     private final Logger logger = LoggerFactory.getLogger(VMLifecycleManager.class);
 
-    public OperationFuture<Operation, Operation> createInstance(VMAddress vmAddress, VMSpecs vmSpecs)
-            throws IOException {
-
+    @Async
+    public Future<Operation> createInstance(VMAddress vmAddress, VMSpecs vmSpecs)
+            throws IOException, ExecutionException, InterruptedException, TimeoutException {
         try (InstancesClient instancesClient = InstancesClient.create()) {
             var disk = diskProvider.createDisk(defaultDiskName, vmSpecs.imageFamily(), vmSpecs.diskSizeGB());
 
@@ -41,18 +47,23 @@ public class VMLifecycleManager {
 
             logger.info(String.format("Creating instance: %s at %s %n", vmAddress.vmName(), vmAddress.zone()));
 
-            return instancesClient.insertAsync(insertInstanceRequest);
+            var futureOperation = instancesClient.insertAsync(insertInstanceRequest);
+
+            return new AsyncResult<>(futureOperation.get(GCLOUD_TIMEOUT_MINUTES, TimeUnit.MINUTES));
         }
     }
 
-    public OperationFuture<Operation, Operation> deleteInstance(VMAddress vmAddress)
-            throws IOException {
+    @Async
+    public Future<Operation> deleteInstance(VMAddress vmAddress)
+            throws IOException, ExecutionException, InterruptedException, TimeoutException {
         try (var instancesClient = InstancesClient.create()) {
             var deleteInstanceRequest = createDeleteInstanceRequest(vmAddress.project(), vmAddress.zone(), vmAddress.vmName());
 
             logger.info(String.format("Deleting instance: %s ", vmAddress.vmName()));
 
-            return instancesClient.deleteAsync(deleteInstanceRequest);
+            var futureOperation = instancesClient.deleteAsync(deleteInstanceRequest);
+
+            return new AsyncResult<>(futureOperation.get(GCLOUD_TIMEOUT_MINUTES, TimeUnit.MINUTES));
         }
     }
 
@@ -73,41 +84,5 @@ public class VMLifecycleManager {
                 .build();
     }
 
-    @PostConstruct
-    // testując tutaj rzuca thread pool rzuca exception, ale request sie wykonuje
-    public void init() {
-        var project = "hybrid-text-350213";
-        var zone = "us-west2-a";
-        var networkName = "default";
-        var vmName = "super-instancja";
 
-        var vmAddress = new VMAddress(project, zone, networkName, vmName);
-        var vmSpecs = new VMSpecs("e2-micro", "debian-11", 10);
-
-//        try {
-//            var operation = createInstance(vmAddress, vmSpecs);
-//            var response = operation.get(3, TimeUnit.MINUTES);
-//
-//            if (response.hasError()) {
-//                logger.error("Instance creation failed! " + response);
-//            } else {
-//                logger.info("Operation Status: " + response.getStatus());
-//            }
-//        } catch (Exception exception) {
-//            logger.error("Failed to create instance. " + vmAddress + " " + exception);
-//        }
-
-        try {
-            var operation = deleteInstance(vmAddress);
-            var response = operation.get(3, TimeUnit.MINUTES);
-
-            if (response.hasError()) {
-                logger.error("Instance creation failed! " + response);
-            } else {
-                logger.info("Operation Status: " + response.getStatus());
-            }
-        } catch (Exception exception) {
-            logger.error("Failed to delete instance. " + vmAddress + " " + exception);
-        }
-    }
 }
