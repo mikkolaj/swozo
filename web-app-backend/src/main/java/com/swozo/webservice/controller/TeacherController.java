c690b0d8130d7b013300cad5247e4fd1515e04a8
Adam <adamniemiec862@gmail.com>
2022-06-04 19:04:16 +0200

    [SWOZO-14] Add CrossOrigin annotation

--- web-app-backend/src/main/java/com/swozo/webservice/controller/TeacherController.java
+++ web-app-backend/src/main/java/com/swozo/webservice/controller/TeacherController.java
@@ -4,10 +4,7 @@ import com.swozo.databasemodel.Activity;
 import com.swozo.databasemodel.Course;
 import com.swozo.security.AccessToken;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 import java.util.LinkedList;
@@ -15,6 +12,7 @@ import java.util.LinkedList;
 //tu nazwa wydaje mi sie że do zmiany, ale nwm jak nazwać taki najogólniejszy controller,
 //który ma obsługiwać listowanie i dodawanie/usuwanie kursów
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class TeacherController {
     private final String teacherService = "course service";
 
