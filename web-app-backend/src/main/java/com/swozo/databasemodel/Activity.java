da311dae653141f8a24f8eaed601752b3a57672d
Adam <adamniemiec862@gmail.com>
2022-06-04 12:07:54 +0200

    [SWOZO-12] append requested changes

--- web-app-backend/src/main/java/com/swozo/databasemodel/Activity.java
+++ web-app-backend/src/main/java/com/swozo/databasemodel/Activity.java
@@ -4,8 +4,11 @@ import lombok.*;
 import net.minidev.json.annotate.JsonIgnore;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedList;
+import java.util.List;
 
 @Entity
 @Table(name = "Activities")
@@ -16,7 +19,12 @@ import java.util.LinkedList;
 @ToString
 public class Activity extends BaseEntity {
     private String name;
-    private String dateTime;
+    private String description;
+    private LocalDateTime dateTime;
+    private String instruction;
+
+    @ElementCollection
+    private List<String> links = new ArrayList<>();
 
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinColumn(name = "module_id")
--- web-app-backend/src/main/java/com/swozo/databasemodel/Course.java
+++ web-app-backend/src/main/java/com/swozo/databasemodel/Course.java
@@ -4,6 +4,7 @@ import com.swozo.databasemodel.users.User;
 import lombok.*;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
@@ -17,6 +18,9 @@ import java.util.LinkedList;
 @ToString
 public class Course extends BaseEntity {
     private String name;
+    private String subject;
+    private String description;
+    private LocalDateTime creationTime = LocalDateTime.now();
 
     //FetchType.LAZY - we won't need downloading classes list everytime e.g. in courses view
     @OneToMany(fetch = FetchType.LAZY)
