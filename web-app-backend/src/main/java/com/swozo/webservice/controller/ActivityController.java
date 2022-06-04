a98906e0ffe0ebb667b9354d495e368abbc9fe1f
Adam <adamniemiec862@gmail.com>
2022-06-04 18:49:05 +0200

    [SWOZO-14] Add basic course components

--- web-app-backend/src/main/java/com/swozo/webservice/controller/ActivityController.java
+++ web-app-backend/src/main/java/com/swozo/webservice/controller/ActivityController.java
@@ -1,4 +1,4 @@
-package com.swozo.webservice;
+package com.swozo.webservice.controller;
 
 import com.swozo.databasemodel.Activity;
 import com.swozo.databasemodel.Course;
--- web-app-backend/src/main/java/com/swozo/webservice/controller/CourseController.java
+++ web-app-backend/src/main/java/com/swozo/webservice/controller/CourseController.java
@@ -1,10 +1,12 @@
-package com.swozo.webservice;
+package com.swozo.webservice.controller;
 
 import com.swozo.databasemodel.Activity;
 import com.swozo.databasemodel.Course;
 import com.swozo.databasemodel.users.User;
 import com.swozo.security.AccessToken;
+import com.swozo.webservice.service.CourseService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,33 +19,38 @@ import static com.swozo.config.SwaggerConfig.ACCESS_TOKEN;
 @RequestMapping("/course")
 @SecurityRequirement(name = ACCESS_TOKEN)
 public class CourseController {
-    private final String courseService = "course service";
+    @Autowired
+    private final CourseService courseService;
 
-//    tu nwm do czego to jeszcze będzie służyć, ale pewnie sie przyda
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    //    tu nwm do czego to jeszcze będzie służyć, ale pewnie sie przyda
     @GetMapping("/course_info_by_id")
     @PreAuthorize("hasRole('TEACHER')")
-    public Course getCourseJson(AccessToken token){
+    public Course getCourseJson(AccessToken token) {
         System.out.println("course  info getter");
         return new Course();
     }
 
     @GetMapping("/activity_list")
     @PreAuthorize("hasRole('TEACHER')")
-    public Collection<Activity> getActivityList(AccessToken token){
+    public Collection<Activity> getActivityList(AccessToken token) {
         System.out.println("activity list");
-        return new LinkedList<Activity>();
+        return new LinkedList<>();
     }
 
     @PostMapping("/add_activity")
     @PreAuthorize("hasRole('TEACHER')")
-    public String addActivityToCourse(AccessToken token){
+    public String addActivityToCourse(AccessToken token) {
         System.out.println("creating new activity inside course");
         return "activity_id";
     }
 
     @DeleteMapping("/delete_activity")
     @PreAuthorize("hasRole('TEACHER')")
-    public String deleteActivityFromCourse(AccessToken token){
+    public String deleteActivityFromCourse(AccessToken token) {
         System.out.println("deleting activity from course");
         return "activity deleted";
     }
@@ -51,36 +58,30 @@ public class CourseController {
 
     @GetMapping("/student_list")
     @PreAuthorize("hasRole('TEACHER')")
-    public Collection<User> getStudentList(AccessToken token){
+    public Collection<User> getStudentList(AccessToken token) {
         System.out.println("students list");
-        return new LinkedList<User>();
+        return new LinkedList<>();
     }
 
     @GetMapping("/student_list_inside_course")
     @PreAuthorize("hasRole('TEACHER')")
-    public Collection<User> getStudentListFromCourse(AccessToken token){
+    public Collection<User> getStudentListFromCourse(AccessToken token) {
         System.out.println("students list inside course");
-        return new LinkedList<User>();
+        return new LinkedList<>();
     }
 
     @PostMapping("/add_student")
     @PreAuthorize("hasRole('TEACHER')")
-    public String addStudentToCourse(AccessToken token){
+    public String addStudentToCourse(AccessToken token) {
         System.out.println("creating new activity inside course");
         return "student added";
     }
 
     @DeleteMapping("/remove_student")
     @PreAuthorize("hasRole('TEACHER')")
-    public String removeStudentFromCourse(AccessToken token){
+    public String removeStudentFromCourse(AccessToken token) {
         System.out.println("removing student from course");
         return "student removed";
     }
-
-
-
-
-
-
 
 }
