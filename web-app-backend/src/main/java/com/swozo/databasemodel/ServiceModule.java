af965582a0539fe0b73149349f5fe2bd3809825a
Adam <adamniemiec862@gmail.com>
2022-06-04 14:38:02 +0200

    [SWOZO-12] rename module

--- web-app-backend/src/main/java/com/swozo/databasemodel/ServiceModule.java
+++ web-app-backend/src/main/java/com/swozo/databasemodel/ServiceModule.java
@@ -6,13 +6,13 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "Modules")
+@Table(name = "ServiceModules")
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 @ToString
-public class Module extends BaseEntity {
+public class ServiceModule extends BaseEntity {
     private String name;
     private String instructionsFromTechnicalTeacher;
 
