f1634071e47b49bc608dd5371c24d0df5503f351
Adam <adamniemiec862@gmail.com>
2022-06-04 18:49:05 +0200

    [SWOZO-14] Add basic course components

--- web-app-backend/src/main/java/com/swozo/webservice/controller/TeacherController.java
+++ web-app-backend/src/main/java/com/swozo/webservice/controller/TeacherController.java
@@ -1,4 +1,4 @@
-package com.swozo.webservice;
+package com.swozo.webservice.controller;
 
 import com.swozo.databasemodel.Activity;
 import com.swozo.databasemodel.Course;
@@ -7,32 +7,34 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
 import java.util.LinkedList;
 
 //tu nazwa wydaje mi sie że do zmiany, ale nwm jak nazwać taki najogólniejszy controller,
 //który ma obsługiwać listowanie i dodawanie/usuwanie kursów
+@RestController
 public class TeacherController {
     private final String teacherService = "course service";
 
     @GetMapping("/course_list")
     @PreAuthorize("hasRole('TEACHER')")
-    public Collection<Course> getActivityList(AccessToken token){
+    public Collection<Course> getActivityList(AccessToken token) {
         System.out.println("course list");
-        return new LinkedList<Course>();
+        return new LinkedList<>();
     }
 
     @PostMapping("/add_course")
     @PreAuthorize("hasRole('TEACHER')")
-    public String addCourse(AccessToken token){
+    public String addCourse(AccessToken token) {
         System.out.println("creating new course");
         return "course_id";
     }
 
     @DeleteMapping("/deleting_course")
     @PreAuthorize("hasRole('TEACHER')")
-    public String deleteCourse(AccessToken token){
+    public String deleteCourse(AccessToken token) {
         System.out.println("deleting new course");
         return "course deleted";
     }
---
+++ web-app-backend/src/main/java/com/swozo/webservice/repository/CourseRepository.java
@@ -0,0 +1,8 @@
+package com.swozo.webservice.repository;
+
+import com.swozo.databasemodel.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// JpaRepository<1, 2>    1 - mapped class, 2 - id format
+public interface CourseRepository extends JpaRepository<Course, Long> {
+}
