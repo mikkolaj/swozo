cbd443ac1730ac614c6676dcea0d400b96712e26
mikolaj <bulmikolaj@interia.pl>
2022-06-14 22:45:31 +0200

    [SWOZO-11] better formatter

--- orchestrator/src/main/java/com/swozo/orchestrator/cloud/software/HttpLinkFormatter.java
+++ orchestrator/src/main/java/com/swozo/orchestrator/cloud/software/HttpLinkFormatter.java
@@ -7,7 +7,7 @@ public class HttpLinkFormatter {
     private static final String HTTP_LINK_TEMPLATE = "http://%s:%s%s";
 
     public static String getLink(String ip, String port) {
-        return String.format(HTTP_LINK_TEMPLATE, ip, port, "");
+        return getLink(ip, port, "");
     }
 
     public static String getLink(String ip, String port, String resourcePath) {
