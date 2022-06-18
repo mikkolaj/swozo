36fcc0d4c8a8b279b56392b41a9e6859394a9945
Fl0k3n <flok3n@gmail.com>
2022-06-18 14:19:27 +0200

    [SWOZO-21] minor fix, upgrade version

--- swozo-commons/src/main/java/com/swozo/repository/UserRepository.java
+++ swozo-commons/src/main/java/com/swozo/repository/UserRepository.java
@@ -2,11 +2,9 @@ package com.swozo.repository;
 
 import com.swozo.databasemodel.users.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
-@Component
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 }
