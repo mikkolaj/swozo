806c3a3ae4e24f336ce2010e7444ade48ed73d15
Fl0k3n <flok3n@gmail.com>
2022-06-19 11:37:27 +0200

    [SWOZO-18] generate code

--- web-app-frontend/src/api/initialize-apis.ts
+++ web-app-frontend/src/api/initialize-apis.ts
@@ -1,20 +1,49 @@
-import { AuthControllerApi, Configuration, ExampleControllerApi } from 'api';
+import {
+    AuthControllerApi,
+    Configuration,
+    CourseControllerApi,
+    Middleware,
+    ResponseContext,
+    ServiceModuleControllerApi,
+} from 'api';
 import { getAccessToken } from 'services/features/auth/auth';
 
 type Apis = {
     authApi: AuthControllerApi;
-    exampleApi: ExampleControllerApi;
+    courseApi: CourseControllerApi;
+    serviceModuleApi: ServiceModuleControllerApi;
 };
+
+class ErrorPreprocessorMiddleware implements Middleware {
+    async post({ response, url }: ResponseContext): Promise<Response | void> {
+        if (!response.ok) {
+            console.error(`Request for: ${url} failed with status: ${response.status}`);
+            let errorBody;
+            try {
+                // TODO check Content-Type if correct body might not be json
+                errorBody = await response.json();
+            } catch (err) {
+                throw new Error('Failed to parse error body');
+            }
+
+            throw errorBody;
+        }
+
+        return response;
+    }
+}
 
 export const initializeApis = (): Apis => {
     // TODO config for dev/prod from env
     const configuration = new Configuration({
         accessToken: getAccessToken,
+        middleware: [new ErrorPreprocessorMiddleware()],
     });
 
     return {
         authApi: new AuthControllerApi(configuration),
-        exampleApi: new ExampleControllerApi(configuration),
+        courseApi: new CourseControllerApi(configuration),
+        serviceModuleApi: new ServiceModuleControllerApi(configuration),
     };
 };
 
