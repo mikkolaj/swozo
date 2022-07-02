1eb15a821562e200a8560c557f096ce768c44af1
mikkolaj <bulmikolaj@interia.pl>
2022-07-02 17:00:28 +0200

    [SWOZO-23] # moved gradle settings and binaries to uppermost directory # linked swozo-commons using project() instead of hardcoded version # fixed tests by adding preview support to orchestrator and including in-memory db in place of postgres in web-app-backend # disabled DataSourceAutoConfig in orchestrator until we start using db connections # switched to using lombok gradle plugin as it's the recommended way # added test properties in both orchestrator and web-app-backend

--- web-app-backend/src/test/java/com/swozo/SwozoApplicationTests.java
+++ web-app-backend/src/test/java/com/swozo/SwozoApplicationTests.java
@@ -2,12 +2,14 @@ package com.swozo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
 class SwozoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
