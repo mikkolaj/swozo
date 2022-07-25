e09344fe3856d06504168d1a0bedc6c06a2e2a11
mikolaj <bulmikolaj@interia.pl>
2022-07-25 21:43:54 +0200

    [SWOZO-23] changes after CR

--- web-app-backend/src/test/java/com/swozo/SwozoApplicationTests.java
+++ web-app-backend/src/test/java/com/swozo/SwozoApplicationTests.java
@@ -2,10 +2,10 @@ package com.swozo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@TestPropertySource(locations = "classpath:application-test.properties")
+@ActiveProfiles("test")
 class SwozoApplicationTests {
 
     @Test
