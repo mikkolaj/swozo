ce9cec9357b7df7b4025acd01fa64d989118ca03
mikkolaj <bulmikolaj@interia.pl>
2022-10-13 22:53:24 +0200

    [SWOZO-27] removed jsontypeinfo

--- swozo-commons/src/main/java/com/swozo/model/scheduling/ScheduleRequest.java
+++ swozo-commons/src/main/java/com/swozo/model/scheduling/ScheduleRequest.java
@@ -1,7 +1,6 @@
 package com.swozo.model.scheduling;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.swozo.model.scheduling.properties.Psm;
 import com.swozo.model.scheduling.properties.ScheduleType;
 import com.swozo.model.scheduling.properties.ServiceLifespan;
@@ -9,7 +8,6 @@ import com.swozo.model.scheduling.properties.ServiceLifespan;
 import java.util.Map;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-@JsonTypeInfo(use = JsonTypeInfo.Id.NAME)
 public record ScheduleRequest(
         ServiceLifespan serviceLifespan,
         Psm psm,
