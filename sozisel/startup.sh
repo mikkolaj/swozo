3190634959b4c5bf4666de495a3a906f274072b5
Detuscher862 <adamniemiec862@gmail.com>
2022-10-22 21:21:54 +0200

    Finish Dockerfile + startup, add docker-compose

--- sozisel/startup.sh
+++ sozisel/startup.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-cd backend
-docker-compose -f docker-compose.yml up --detach
-docker-compose -f jitsi-compose.yml --env-file .jitsi_env up --detach
-cd sozisel
+cd backend/sozisel
+#docker-compose -f docker-compose.yml up --detach
+#docker-compose -f jitsi-compose.yml --env-file .jitsi_env up --detach
 mix deps.get
-apk add erlang-dev
-mix local.rebar --force
 mix ecto.setup
 mix phx.server
