ffc0a54177596d3406d986276eddbea17290b2b3
Adam <adamniemiec862@gmail.com>
2022-10-26 22:04:43 +0200

    It actually does work

--- sozisel/backend/sozisel/config/dev.exs
+++ sozisel/backend/sozisel/config/dev.exs
@@ -5,7 +5,7 @@ config :sozisel, Sozisel.Repo,
   username: "postgres",
   password: "postgres",
   database: "sozisel_dev",
-  hostname: "localhost",
+  hostname: "postgres",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
--- sozisel/startup.sh
+++ sozisel/startup.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-cd backend/sozisel
-#docker-compose -f docker-compose.yml up --detach
-#docker-compose -f jitsi-compose.yml --env-file .jitsi_env up --detach
-mix deps.get
+cd backend/sozisel/
 mix ecto.setup
 mix phx.server
