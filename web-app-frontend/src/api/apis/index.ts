41552a0db78682a7a5b349e17f64a82da58a66af
Fl0k3n <flok3n@gmail.com>
2022-11-07 14:28:59 +0100

    [SWOZO-37] generate vm + policies api spec

--- web-app-frontend/src/api/apis/index.ts
+++ web-app-frontend/src/api/apis/index.ts
@@ -4,6 +4,8 @@ export * from './ActivityControllerApi';
 export * from './AuthControllerApi';
 export * from './CourseControllerApi';
 export * from './FileControllerApi';
+export * from './PolicyControllerApi';
 export * from './SandboxControllerApi';
 export * from './ServiceModuleControllerApi';
 export * from './UserControllerApi';
+export * from './VmControllerApi';
